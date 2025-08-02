f7e0db11db750834f022c9226ba577189e5c83a3
Dollypee48 <akinolad22@gmail.com>
2025-08-02 20:29:30 +0100

    eroor solving

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-// src/server.js
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -9,20 +9,20 @@ const bookmarkRoutes = require("../routes/bookmarks");
 const tagRoutes = require("../routes/tags");
 const userRoutes = require("../routes/users");
 
-// Load environment variables
+
 dotenv.config();
 
-// Initialize app
+
 const app = express();
 
-// Middleware
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// Connect to DB
+
 connectDB();
 
-// API Routes
+
 app.use("/api/bookmarks", bookmarkRoutes);
 app.use("/api/tags", tagRoutes);
 app.use("/api/users", userRoutes);
@@ -31,6 +31,6 @@ app.get("/", (req, res) => {
   res.send("Bookmark app backend is running ✅");
 });
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
