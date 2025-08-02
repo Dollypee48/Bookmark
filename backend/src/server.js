defa597b0e25751270a23221a74a036ff246d4f3
Dollypee48 <akinolad22@gmail.com>
2025-08-02 22:21:38 +0100

    still solving error

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,33 +4,31 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("../config/db");
 const corsOptions = require("../config/corsOptions");
-
 const bookmarkRoutes = require("../routes/bookmarks");
 const tagRoutes = require("../routes/tags");
 const userRoutes = require("../routes/users");
 
+require("dotenv").config(); 
+
 
 dotenv.config();
-
+connectDB();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-
-connectDB();
-
-
-app.use("/api/bookmarks", bookmarkRoutes);
-app.use("/api/tags", tagRoutes);
-app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Bookmark app backend is running ✅");
 });
 
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/bookmarks", bookmarkRoutes);
+app.use("/api/tags", tagRoutes);
+app.use("/api/users", userRoutes);
+
+
+
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
