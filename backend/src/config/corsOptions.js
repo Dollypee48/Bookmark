f05ada2b7db84d696059378445d1404c5c0544fb
Dollypee48 <akinolad22@gmail.com>
2025-08-02 20:12:48 +0100

    change the url

--- backend/src/config/corsOptions.js
+++ backend/src/config/corsOptions.js
@@ -1,5 +1,5 @@
 
-const allowedOrigins = ["http://localhost:5173"]; 
+const allowedOrigins = ["http://localhost:5173", "https://bookmark-axvj.vercel.app"]; 
 
 const corsOptions = {
   origin: (origin, callback) => {
